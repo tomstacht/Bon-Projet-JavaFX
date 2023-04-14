73da72127e8ac43433daccf09d2ec0710a979a28
rapha <raphaelhilt@edu.ece.fr>
2023-04-15 00:54:57 +0200

    checkbox et fonction pour master

--- src/main/java/com/example/bonprojetjavafx/HelloController.java
+++ src/main/java/com/example/bonprojetjavafx/HelloController.java
@@ -42,6 +42,46 @@ public class HelloController {
 
     @FXML
     private CheckBox MyCheckBoxHotel;
+    @FXML
+    private CheckBox MyCheckBoxVilla;
+    @FXML
+    private CheckBox MyCheckBoxChalet;
+    @FXML
+    private CheckBox MyCheckBoxAppartement;
+    @FXML
+    private CheckBox MyCheckBoxPaschere;
+    @FXML
+    private CheckBox MyCheckBoxMoyen;
+    @FXML
+    private CheckBox MyCheckBoxChere;
+    @FXML
+    private CheckBox MyCheckBox123Etoiles;
+    @FXML
+    private CheckBox MyCheckBox45Etoiles;
+    @FXML
+    private CheckBox MyCheckBoxP_D;
+    @FXML
+    private CheckBox MyCheckBoxRestaurant;
+    @FXML
+    private CheckBox MyCheckBoxNoteClientNul;
+    @FXML
+    private CheckBox MyCheckBoxNoteClientBien;
+    @FXML
+    private CheckBox MyCheckBoxPiscine;
+    @FXML
+    private CheckBox MyCheckBoxWifi;
+    @FXML
+    private CheckBox MyCheckBoxClim;
+    @FXML
+    private CheckBox MyCheckBoxFumeur;
+    @FXML
+    private CheckBox MyCheckBoxAnimaux;
+    @FXML
+    private CheckBox MyCheckBoxParking;
+    @FXML
+    private CheckBox MyCheckBoxLocalSki;
+    @FXML
+    private CheckBox MyCheckBoxCheminee ;
 
     public void ChangeHotel(ActionEvent eventHotel){
         if(MyCheckBoxHotel.isSelected()){
@@ -51,5 +91,52 @@ public class HelloController {
             System.out.println("Hotel=0");
         }
     }
-
+    public void ChangeVilla(ActionEvent eventVilla){
+        if(MyCheckBoxVilla.isSelected()){
+            System.out.println("Villa=1");
+        }
+        else{
+            System.out.println("Villa=0");
+        }
+    }
+    public void ChangeChalet(ActionEvent eventChalet){
+        if(MyCheckBoxChalet.isSelected()){
+            System.out.println("Chalet=1");
+        }
+        else{
+            System.out.println("Chalet=0");
+        }
+    }
+    public void ChangeAppart(ActionEvent eventAppart){
+        if(MyCheckBoxAppartement.isSelected()){
+            System.out.println("Appart=1");
+        }
+        else{
+            System.out.println("Appart=0");
+        }
+    }
+    public void ChangePasChere(ActionEvent eventPasChere){
+        if(MyCheckBoxPaschere.isSelected()){
+            System.out.println("PasChere=1");
+        }
+        else{
+            System.out.println("PasChere=0");
+        }
+    }
+    public void ChangeMoyen(ActionEvent eventMoyen){
+        if(MyCheckBoxMoyen.isSelected()){
+            System.out.println("Moyen=1");
+        }
+        else{
+            System.out.println("Moyen=0");
+        }
+    }
+    public void ChangeChere(ActionEvent eventChere){
+        if(MyCheckBoxChere.isSelected()){
+            System.out.println("Chere=1");
+        }
+        else{
+            System.out.println("Chere=0");
+        }
+    }
 }
