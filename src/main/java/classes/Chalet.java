40ceb5a835baa0ab34a1785506451d62f65a051e
rapha <raphaelhilt@edu.ece.fr>
2023-04-14 18:07:34 +0200

    creation class chalet dans le meme package

---
+++ src/main/java/classes/Chalet.java
@@ -0,0 +1,23 @@
+package classes;
+
+public class Chalet extends Logement{
+
+    private boolean cheminer;
+    private int distance_piste;
+
+    public boolean getCheminer() {
+        return cheminer;
+    }
+    public void setCheminer(boolean cheminer) {
+        this.cheminer = cheminer;
+    }
+
+    public int getDistance_piste() {
+        return distance_piste;
+    }
+    public void setDistance_piste(int distance_piste) {
+        this.distance_piste = distance_piste;
+    }
+    public Chalet() {
+    }
+}
