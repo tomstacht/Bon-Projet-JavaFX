4b75c56c7b0ce2413363847418125052811e61ee
tomstacht <99322762+tomstacht@users.noreply.github.com>
2023-04-15 00:54:20 +0200

    interface log in avec bouton fonctionnel / manque le MySQL

--- src/main/java/com/example/bonprojetjavafx/HelloController.java
+++ src/main/java/com/example/bonprojetjavafx/HelloController.java
@@ -3,6 +3,7 @@ package com.example.bonprojetjavafx;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
@@ -10,6 +11,9 @@ import javafx.scene.Parent;
 import java.io.IOException;
 import javafx.scene.control.Button;
 import javafx.stage.StageStyle;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+
 
 public class HelloController {
     @FXML
@@ -42,6 +46,19 @@ public class HelloController {
 
     @FXML
     private CheckBox MyCheckBoxHotel;
+    @FXML
+    private Label loginMessageLabel;
+    @FXML
+    private TextField UsernameTextField;
+    @FXML
+    private TextField PasswordTextField;
+    public void loginButtonOnAction(ActionEvent e){
+        if(UsernameTextField.getText().isBlank() == false && PasswordTextField.getText().isBlank() == false){
+            loginMessageLabel.setText("Vous avez essayé de vous connecter !");
+        }else{
+            loginMessageLabel.setText("Veuillez entrer votre adresse mail et votre mot de passe:");
+        }
+    }
 
     public void ChangeHotel(ActionEvent eventHotel){
         if(MyCheckBoxHotel.isSelected()){
