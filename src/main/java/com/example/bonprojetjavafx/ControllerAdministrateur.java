d78ac261f35fd2e89b52362d946ac6d6e62c83cc
tomstacht <99322762+tomstacht@users.noreply.github.com>
2023-04-16 01:28:11 +0200

    Page choix admin 3.0

--- src/main/java/com/example/bonprojetjavafx/ControllerAdministrateur.java
+++ src/main/java/com/example/bonprojetjavafx/ControllerAdministrateur.java
@@ -44,7 +44,7 @@ public class ControllerAdministrateur {
         Scene scene = new Scene(root);
         Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         Stage newStage = new Stage();
-        newStage.setTitle("GetAway/ADMIN/Choix.com");
+        newStage.setTitle("GetAway/ADMIN/ChoixAction.com");
         newStage.setScene(scene);
         newStage.show();
         currentStage.close();
