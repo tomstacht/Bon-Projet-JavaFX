e5983ad0b3f9d7255e663e9ee3d816d1998434d8
tomstacht <99322762+tomstacht@users.noreply.github.com>
2023-04-16 00:24:10 +0200

    Debut page choix admin

--- src/main/java/com/example/bonprojetjavafx/ControllerAdministrateur.java
+++ src/main/java/com/example/bonprojetjavafx/ControllerAdministrateur.java
@@ -24,12 +24,27 @@ public class ControllerAdministrateur {
 
     @FXML
     private void BoutonAnnulerAdmin(ActionEvent event) throws IOException{
+
         FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
         Parent root = loader.load();
         Scene scene = new Scene(root);
         Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         Stage newStage = new Stage();
         newStage.setTitle("GetAway.com");
+        newStage.setScene(scene);
+        newStage.show();
+        currentStage.close();
+    }
+
+    @FXML
+    private void BoutonConnexionAdmin(ActionEvent event) throws IOException{
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("page-choix-admin.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Stage newStage = new Stage();
+        newStage.setTitle("GetAway/ADMIN/Choix.com");
         newStage.setScene(scene);
         newStage.show();
         currentStage.close();
