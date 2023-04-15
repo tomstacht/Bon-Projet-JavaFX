b8fde1ab2a755b26c90fe11025afb05bb8ca56f3
tomstacht <99322762+tomstacht@users.noreply.github.com>
2023-04-15 12:35:58 +0200

    Ajout fichiers pour s'inscrire

---
+++ src/main/java/com/example/bonprojetjavafx/ControllerInscription.java
@@ -0,0 +1,18 @@
+package com.example.bonprojetjavafx;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.scene.Parent;
+import java.io.IOException;
+import javafx.scene.control.Button;
+import javafx.stage.StageStyle;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+public class ControllerInscription {
+
+}
