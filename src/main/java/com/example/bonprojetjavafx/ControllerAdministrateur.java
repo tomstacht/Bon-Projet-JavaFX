26fe3f53cb1c5f89c53ffd8f0e1b2142d20a1064
tomstacht <99322762+tomstacht@users.noreply.github.com>
2023-04-15 23:39:03 +0200

    debut page admin

---
+++ src/main/java/com/example/bonprojetjavafx/ControllerAdministrateur.java
@@ -0,0 +1,25 @@
+package com.example.bonprojetjavafx;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.Node;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.scene.Parent;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Objects;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.control.Button;
+import javafx.stage.StageStyle;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.DatePicker;
+
+public class ControllerAdministrateur {
+
+}
