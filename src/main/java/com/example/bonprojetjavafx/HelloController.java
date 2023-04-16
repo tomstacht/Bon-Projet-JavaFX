330bd268a2028ad23bb83b02e7cee403183bc881
tomstacht <99322762+tomstacht@users.noreply.github.com>
2023-04-16 10:53:10 +0200

    resolution conflits

--- src/main/java/com/example/bonprojetjavafx/HelloController.java
+++ src/main/java/com/example/bonprojetjavafx/HelloController.java
@@ -60,6 +60,18 @@ public class HelloController {
         currentStage.close();
     }
     @FXML
+    private void retourPremierePageMenu(ActionEvent event) throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Stage newStage = new Stage();
+        newStage.setTitle("GetAway.com");
+        newStage.setScene(scene);
+        newStage.show();
+        currentStage.close();
+    }
+    @FXML
     private void BoutonActionSeConnecter(ActionEvent event) throws IOException{
         FXMLLoader loader = new FXMLLoader(getClass().getResource("page-connexion.fxml"));
         Parent root = loader.load();
